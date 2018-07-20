faf3c22fbf45a7574afa86a40d2c06b8d8710b4c
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:20:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,29 +1,35 @@
 class PigLatinizer
 
-  def piglatinize(string)
-    string = string.split('')
-    if string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[2].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      second = string.shift
-      third = string.shift
-      "#{string.join}#{first}#{second}#{third}ay"
-    elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/) && string[1].downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      second = string.shift
-      "#{string.join}#{first}#{second}ay"
-    elsif string.first.downcase.match(/[bcdfghjklmnpqrstvwxyz]/)
-      first = string.shift
-      "#{string.join}#{first}ay"
-    elsif string.first.downcase.match(/[aeoui]/)
-      "#{string.join}way"
+  def piglatinize(word)
+    alphabet = ("A".."z").to_a
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    consonants = alphabet - vowels
+    unusable = ["i", "a", "an", "in", "on", "and"]
+    word.gsub!(/[^a-z0-9\s]/i, '')
+    word = word.split("")
+   # ["k", "i", "t", "t", "e", "n"]
+
+    if unusable.include?(word.join) || word.size <= 1
+      word.join
+    elsif vowels.include?(word[0]) && word.size > 1
+      word.join + "ay"
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      word = word.rotate.rotate.rotate << "ay"
+     word.join
+    elsif consonants.include?(word[0] && word[1])
+      word.rotate.rotate.join + "ay"
     else
-      string.join
+      word.rotate.join + "ay"
     end
+
+end
+
+  def to_pig_latin(words)
+    result = words.split(" ").map do |word|
+      piglatinize(word)
+    end
+    result.join(" ")
   end
 
-  def to_pig_latin(sentence)
-    sentence.split(' ').map do |value|
-      self.piglatinize(value)
-    end.join(' ')
-  end
+
 end
