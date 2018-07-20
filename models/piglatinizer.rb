7ef226b65c1b9b0b3efb82dee8e549e6221f8931
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:34:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -42,7 +42,7 @@ def piglatinize(sent)
           end
           # translation of consonant words without qu
           if not qu
-              translation = words[i][count..words[i].length] + translation + 'way'
+              translation = words[i][count..words[i].length] + translation + 'ay'
               result.push(translation)
           end
       end
