043ea84cb33f3f87d194eb692a5e678cf783ce64
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:43:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,45 +1,64 @@
 class PigLatinizer
 
-  def piglatinize(x)
+  def translate(str)
+      # Vowels to consider
+      vowels = ["a", "e", "i", "o", "u"]
+      
+      # Special cases to consider
+      two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
+      three_letter_consonants = ["thr", "sch", "squ"]
 
-      if x.index(/[aeiou]/) == 0
-        x = x + "ay"
-      elsif x[0..1] == "qu"
-        first = x.chr
-        x.reverse!.chop!.reverse!
-        second = x.chr
-        x.reverse!.chop!.reverse!
-        x = x + first + second + "ay"
-      elsif x.index(/[aeiou]/) == 1
-        first = x.chr
-        x.reverse!.chop!.reverse!
-        x = x + first + "ay"
-      elsif x[1..2] == "qu"
-        first = x.chr
-        x.reverse!.chop!.reverse!
-        second = x.chr
-        x.reverse!.chop!.reverse!
-        third = x.chr
-        x.reverse!.chop!.reverse!
-        x = x + first + second + third +"ay"
-      elsif x.index(/[aeiou]/) == 2
-        first = x.chr.chr
-        x.reverse!.chop!.reverse!
-        second = x.chr
-        x.reverse!.chop!.reverse!
-        x = x + first + second + "ay"
-      elsif x.index(/[aeiou]/) == 3
-        first = x.chr
-        x.reverse!.chop!.reverse!
-        second = x.chr
-        x.reverse!.chop!.reverse!
-        third = x.chr
-        x.reverse!.chop!.reverse!
-        x = x + first + second + third +"ay"
-      else
-        x
-      end
-    end
+      # Seperate each word from the phrase given
+      words = str.split(" ")
+      
+      #Location for processed words
+      result = [];
+      
+      
+      
+      words.each do |word|
+          # Words that start with a vowels
+          if vowels.include? word[0]
+              result.push word << 'ay'
+              
+          # Words that start with a consonant
+          else
+              # Check for special consonants
+              if three_letter_consonants.include? word[0] + word[1] + word[2]
+                  # Slice off first three letters
+                  first_three_letters = word.slice!(0,3)
+                  
+                  # Add letters to end of word with 'ay'
+                  result.push word << first_three_letters << 'ay'
+                  
+              elsif  two_letter_consonants.include? word[0] + word[1]
+                  # Slice off first two letters
+                  first_two_letters = word.slice!(0,2)
+                  
+                  # Add the letters to end of word with 'ay'
+                  result.push word << first_two_letters << 'ay'
+                  
+              else
+                  # Slice off first letter...
+                  first_letter = word.slice!(0)
+                  
+                  # Add first letter to end of word with 'ay'
+                  result.push word << first_letter << 'ay'
+              end #End of special consonant check
+      
+          end #End of vowel check
+          
+          
+          
+      end #End of words.each
+      
+      
+      
+      #Present the processed words as a single string
+      return result.join(" ")
+
+  end #End of translate function
+
 
 
   # def translate_words(multi_words)
