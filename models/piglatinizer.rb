a8c716ad9b7cd6507291ee1b540be347c1593545
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:25:09 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,28 +1,20 @@
 class PigLatinizer
 
-  def piglatinize(word)
-    alphabet = ("A".."z").to_a
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    consonants = alphabet - vowels
-    unusable = ["i", "a", "an", "in", "on", "and"]
-    word.gsub!(/[^a-z0-9\s]/i, '')
-    word = word.split("")
-   # ["k", "i", "t", "t", "e", "n"]
-
-    if unusable.include?(word.join) || word.size <= 1
-      word.join
-    elsif vowels.include?(word[0]) && word.size > 1
-      word.join + "ay"
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-      word = word.rotate.rotate.rotate << "ay"
-     word.join
-    elsif consonants.include?(word[0] && word[1])
-      word.rotate.rotate.join + "ay"
-    else
-      word.rotate.join + "ay"
+  def translate(word)
+    words=word.split(" ")
+    words.each do |x|
+      if ["a","e","i","o","u"].include?x[0,1]
+        x << ("ay")
+      else
+        until ["a","e","i","o","u"].include?x[0,1]
+          x << ("#{x[0,1]}")
+          x[0,1]=""
+        end
+        x << ("#{x[0,1]}ay")
+      end
     end
-
-end
+    words.join(" ")
+  end
 
   def to_pig_latin(words)
     result = words.split(" ").map do |word|
