d0c9c11d424705d6440e4ce6cc1c7ad42a0f3f82
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-20 04:26:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,25 @@
 class PigLatinizer
 
   def translate(word)
-    words=word.split(" ")
-    words.each do |x|
-      if ["a","e","i","o","u"].include?x[0,1]
-        x << ("ay")
-      else
-        until ["a","e","i","o","u"].include?x[0,1]
-          x << ("#{x[0,1]}")
-          x[0,1]=""
-        end
-        x << ("#{x[0,1]}ay")
-      end
-    end
-    words.join(" ")
+    Alpha = ('a'..'z').to_a
+Vowels = %w[a e i o u]
+Consonants = Alpha - Vowels
+
+def translate(word)
+  if Vowels.include?(word[0])
+    word + 'ay'
+  elsif Consonants.include?(word[0]) &&
+    Consonants.include?(word[1])
+    word[2..-1] + word[0..1] + 'ay'
+  elsif word[0..1] == "qu"
+    word[2..-1]+"quay"
+  elsif word[0..2] == "squ"
+     word[3..-1]+"squay"
+  else Consonants.include?(word[0])
+    word[1..-1] + word[0..0] + 'ay'
   end
+end
+
 
   def to_pig_latin(words)
     result = words.split(" ").map do |word|
